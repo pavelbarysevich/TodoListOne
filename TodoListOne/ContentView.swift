624497d7a5a01753db6bdd341c8bbc73142e9bfc
Pavel Barysevich <pavelbarysevich@gmail.com>
2026-02-06 15:07:55 +0300

    Initial Commit

---
+++ TodoListOne/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  TodoListOne
+//
+//  Created by Павел Борисевич on 6.02.26.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ TodoListOne/TodoListOneApp.swift
@@ -0,0 +1,17 @@
+//
+//  TodoListOneApp.swift
+//  TodoListOne
+//
+//  Created by Павел Борисевич on 6.02.26.
+//
+
+import SwiftUI
+
+@main
+struct TodoListOneApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
